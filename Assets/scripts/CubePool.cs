57f2b169553a417009c9bc71d38232a66d5a834d
nanofrks <softivyad@gmail.com>
2023-05-05 17:35:15 -0500

    cubePool

---
+++ Assets/scripts/CubePool.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePool : MonoBehaviour
+{
+    private static CubePool instance;
+
+    [SerializeField]
+    private int size = 15;
+
+    [SerializeField]
+    private GameObject cubePrefab;
+    private List<GameObject> instances = new List<GameObject>();
+
+    public static CubePool Instance { get => instance; private set => instance = value; }
+
+    public GameObject Retrieve()
+    {
+        GameObject target = instances[0];
+        target.transform.parent = null;
+        instances.Remove(target);
+        target.SetActive(true);
+
+        return target;
+    }
+
+    public void Recycle(GameObject target)
+    {
+        target.SetActive(false); //apago el gameObject
+        target.transform.position = transform.position;
+        target.transform.rotation = Quaternion.identity;
+        target.transform.parent = transform;
+        instances.Add(target);
+    }
+
+    private void Awake()
+    {
+        //patrón singleton
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cubePrefab != null)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                AddNewInstanceToPool();
+            } 
+        }
+        
+    }
+
+    private void AddNewInstanceToPool()
+    {
+        GameObject newInstance = Instantiate(cubePrefab, transform.position, Quaternion.identity);
+        Recycle(newInstance);
+    }
+
+
+}
