561493cd80f44ae65820dc3aaf5b920ec9425e09
nanofrks <softivyad@gmail.com>
2023-05-05 19:38:13 -0500

    generic pool

--- Assets/scripts/CubePool.cs
+++ Assets/scripts/CubePool.cs
@@ -2,65 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubePool : MonoBehaviour
+public class CubePool : PoolBase<GameObject>
 {
-    private static CubePool instance;
-
-    [SerializeField]
-    private int size = 3;
-
-    [SerializeField]
-    private GameObject cubePrefab;
-    private List<GameObject> instances = new List<GameObject>();
-
-    public static CubePool Instance { get => instance; private set => instance = value; }
-
-    public GameObject Retrieve()
+    
+    protected override void AddNewInstanceToPool()
     {
-        GameObject target = instances[0];
+        GameObject newInstance = CubeFactory.Instance.DeliverNewProduct();
+        Recycle(newInstance);
+    }
+
+    protected override void ProcessTargetToRetrieve(GameObject target)
+    {
         target.transform.parent = null;
         instances.Remove(target);
         target.SetActive(true);
-
-        return target;
     }
 
-    public void Recycle(GameObject target)
+    protected override void ProcessTargetToRecycle(GameObject target)
     {
         target.SetActive(false); //apago el gameObject
         target.transform.position = transform.position;
         target.transform.rotation = Quaternion.identity;
         target.transform.parent = transform;
-        instances.Add(target);
     }
-
-    private void Awake()
-    {
-        //patrón singleton
-        if (instance == null) instance = this;
-        else Destroy(gameObject);
-
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (cubePrefab != null)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                AddNewInstanceToPool();
-            } 
-        }
-        
-    }
-
-    private void AddNewInstanceToPool()
-    {
-        //GameObject newInstance = Instantiate(cubePrefab, transform.position, Quaternion.identity);
-        GameObject newInstance = CubeFactory.Instance.DeliverNewProduct();
-        Recycle(newInstance);
-    }
-
 
 }
---
+++ Assets/scripts/PoolBase.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PoolBase<T> : MonoBehaviour
+{
+    private static PoolBase<T> instance;
+
+    [SerializeField]
+    private int size = 25;
+
+    [SerializeField]
+    private T basePrefab;
+
+    protected List<T> instances = new List<T>();
+
+    public static PoolBase<T> Instance { get => instance; set => instance = value; }
+
+    public virtual T Retrieve()
+    {
+        if (instances.Count < 1)
+        {
+            AddNewInstanceToPool();
+        }
+
+        T target = instances[0];
+        ProcessTargetToRetrieve(target);
+
+        return target;
+    }
+
+    public virtual void Recycle(T target)
+    {
+        ProcessTargetToRecycle(target);
+        instances.Add(target);
+    }
+
+    protected abstract void ProcessTargetToRetrieve(T target);
+    protected abstract void ProcessTargetToRecycle(T target);
+    protected abstract void AddNewInstanceToPool();
+
+    protected void Awake()
+    {
+        //patrón singleton
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+    }
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        if (basePrefab != null)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                AddNewInstanceToPool();
+            }
+        }
+
+    }
+
+}
